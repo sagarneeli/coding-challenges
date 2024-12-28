13e35642340f9192943df8bacb1cd1cff0a6d2ed
Sagar Neeli <sagarneeli1191@gmail.com>
2024-12-28 15:59:39 -0500

    Time: 27 ms (35.33%) | Memory: 26.3 MB (72.97%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -6,6 +6,7 @@ class Solution:
         for R in range(len(nums)):
             if nums[R] in window:
                 return True
+            
             window.add(nums[R])
             if R - L + 1 > k:
                 window.remove(nums[L])
