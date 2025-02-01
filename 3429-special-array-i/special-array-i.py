3ec2fbc4999951edf3807a97ff13062f5df1ac93
Sagar Neeli <sagarneeli1191@gmail.com>
2025-02-01 18:44:13 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (34.27%) - LeetSync

--- 3429-special-array-i/special-array-i.py
+++ 3429-special-array-i/special-array-i.py
@@ -1,16 +1,12 @@
 class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
+        """
+        Time: O(N), where N is number of items in nums
+        Space: O(1)
+        """
         if len(nums) == 1:
             return True
         
-        """
-        We can iterate through the array but from the second element
-        First element will be prev
-        if prev % 2 == 0 and curr % 2 == 0:
-            return False
-        if prev % 2 == 1 and curr % 2 == 1:
-            return False
-        """
         prev = nums[0]
 
         for curr in nums[1:]:
