bcea957f2bff84ad21b019dc5649c7109f79f664
Sagar Neeli <sagarneeli1191@gmail.com>
2025-02-04 11:28:31 -0500

    Time: 22 ms (18.73%) | Memory: 25.1 MB (48.26%) - LeetSync

---
+++ 975-range-sum-of-bst/range-sum-of-bst.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        """
+            TC: expression + explanation => O(N)
+            SC: O(h) h: height of the tree. O(n) is the tree is skewed and when it's balanced is O(log(n))
+            
+        """
+        def dfs(node):
+            nonlocal result
+            if not node:
+                return
+            
+            dfs(node.left)
+            if low <= node.val <= high:
+                result += node.val
+            dfs(node.right)
+                
+        
+        result = 0
+        dfs(root)
+        return result
+        
