1d3b184bc2ede0fa8def809270d6a05710e210c0
Sagar Neeli <sagarneeli1191@gmail.com>
2025-02-04 16:45:20 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (67.65%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.py
+++ 953-reverse-only-letters/reverse-only-letters.py
@@ -3,15 +3,15 @@ class Solution:
         i, j = 0, len(s) - 1
         s = list(s)
 
-        while i <= j:
-            while i < j and not s[i].isalpha():
+        while i < j:
+            if not s[i].isalpha():
                 i += 1
-            while i < j and not s[j].isalpha():
+            elif not s[j].isalpha():
                 j -= 1
-
-            s[i], s[j] = s[j], s[i]
-            i += 1
-            j -= 1
+            else:
+                s[i], s[j] = s[j], s[i]
+                i += 1
+                j -= 1
 
         return "".join(s)
 
