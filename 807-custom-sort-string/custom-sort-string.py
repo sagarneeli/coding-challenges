ec1e880415a6ac04098fa0ec92647e789084da55
Sagar Neeli <sagarneeli1191@gmail.com>
2025-02-07 23:42:03 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (15.02%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -1,5 +1,9 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
+        """
+        Time: O(N), where N is length of order
+        Space: O(M + N), where M is the length of s and N is the length of order
+        """
         mapping = Counter(s)
         result = []
         
