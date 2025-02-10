86670389e09b6ba9bc78c64bb0f17e10ff1abfc6
Sagar Neeli <sagarneeli1191@gmail.com>
2025-02-10 14:51:28 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (27.74%) - LeetSync

--- 3447-clear-digits/clear-digits.py
+++ 3447-clear-digits/clear-digits.py
@@ -1,10 +1,16 @@
 class Solution:
     def clearDigits(self, s: str) -> str:
         """
-        cb34
-           i
+        Complexity Analysis
+        Let n be the length of the string s.
+        
+        Time Complexity: O(n).
+        Like in the previous approach, we iterate over all characters in s and perform constant-time operations, including checks and retrievals of characters in a string. Additionally, the "resize" operation on the string requires O(n) time and therefore the total time complexity of the algorithm is O(n).
 
-        stack = 
+        Space Complexity: O(1).
+        As the input string does not count as auxiliary space, the C++ implementation requires only constant extra space for the variables answerLength and charIndex.
+
+        However, the Java and Python implementations require additional structures (such as a list or a charArray), as they do not provide mutable strings. Since these structures are neither part of the input nor the output of the algorithm, they contribute to its auxiliary space complexity, which is O(n).
         """
         stack = []
 
