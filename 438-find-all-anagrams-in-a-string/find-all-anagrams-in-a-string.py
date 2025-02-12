088e99ad361cf705a58a294408012a9c58d8026b
Sagar Neeli <sagarneeli1191@gmail.com>
2025-02-12 15:02:57 -0500

    Time: 63 ms (49.60%) | Memory: 18.6 MB (5.60%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        char_count_p = Counter(p)
+        missing = len(char_count_p)
+        result = []
+        left, right = 0, 0
+        k = len(p)
+
+        while right < len(s):
+            if right - left == k:
+                ch = s[left]
+                if ch in char_count_p:
+                    char_count_p[ch] += 1
+                    if char_count_p[ch] == 1:
+                        missing += 1
+                left += 1
+
+
+            ch = s[right]
+            if ch in char_count_p:
+                char_count_p[ch] -= 1
+                if char_count_p[ch] == 0:
+                    missing -= 1
+                    print(missing)
+
+            right += 1
+    
+            if missing == 0:
+                result.append(left)
+
+        return result
+        
