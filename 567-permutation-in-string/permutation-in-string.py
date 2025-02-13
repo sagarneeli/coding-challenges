e4c78384c04695f276bb22c3da576bf662e4350b
Sagar Neeli <sagarneeli1191@gmail.com>
2025-02-13 17:44:41 -0500

    Time: 27 ms (47.59%) | Memory: 17.8 MB (84.95%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -8,6 +8,12 @@ class Solution:
         missing = len(s1_map)
         
         while R < len(s2):
+            ch = s2[R]
+            if ch in s1_map:
+                s1_map[ch] -= 1
+                if s1_map[ch] == 0:
+                    missing -= 1
+
             if R - L == k:
                 ch = s2[L]
                 if ch in s1_map:
@@ -15,13 +21,7 @@ class Solution:
                     if s1_map[ch] == 1:
                         missing += 1
                 L += 1
-            
-            ch = s2[R]
-            if ch in s1_map:
-                s1_map[ch] -= 1
-                if s1_map[ch] == 0:
-                    missing -= 1
-            
+                          
             R += 1
 
             if missing == 0:
