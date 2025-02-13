65232b8e3262e752e5e1026fa381eede735ac5aa
Sagar Neeli <sagarneeli1191@gmail.com>
2025-02-13 16:28:19 -0500

    Time: 87 ms (50.93%) | Memory: 18.3 MB (33.90%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -2,29 +2,28 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if not s or not t:
             return ""
-
-        t_count = Counter(t)
-        missing = len(t_count)
-        result_index, result_size = -1, len(s)
+        dict_t = Counter(t)
+        missing = len(dict_t)
         left, right = 0, 0
+        result_index, result_size = -1, len(s)
 
         while right < len(s):
             if missing > 0:
                 ch = s[right]
-                if ch in t_count:
-                    t_count[ch] -= 1
-                    if t_count[ch] == 0:
+                if ch in dict_t:
+                    dict_t[ch] -= 1
+                    if dict_t[ch] == 0:
                         missing -= 1
                 right += 1
             while missing == 0:
                 if right - left <= result_size:
                     result_index, result_size = left, right - left
                 ch = s[left]
-                if ch in t_count:
-                    t_count[ch] += 1
-                    if t_count[ch] == 1:
+                if ch in dict_t:
+                    dict_t[ch] += 1
+                    if dict_t[ch] == 1:
                         missing += 1
                 left += 1
-
-        return s[result_index : result_index + result_size] if result_size != -1 else ""
+        print(result_index, result_size)
+        return s[result_index: result_size + result_index] if result_index != -1 else ""
         
