ed316418409dcd3ab797193d45911eaa736cd2c2
Sagar Neeli <sagarneeli1191@gmail.com>
2025-02-13 12:22:40 -0500

    new code solutioons

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.py
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.py
@@ -1,11 +1,29 @@
 class Solution:
     def removeOccurrences(self, s: str, part: str) -> str:
         """
-        Time Complexity: Each replace operation scans s for the substring part, which takes O(n) time in the worst case, where n is the length of s. If there are k occurrences of part in s, the overall time complexity is O(n * k).
-        Space Complexity: The space complexity is O(1) for the in-place modification of the string, but since strings are immutable in Python, each replace operation creates a new string. Therefore, the space complexity is O(n) due to the storage of intermediate strings.
-        """
-        while part in s:
-            s = s.replace(part, "", 1)
+        Time Complexity:
+        Each replace operation scans s for the substring part,
+        which takes O(n) time in the worst case, where n is the length of s.
+        If there are k occurrences of part in s, the overall time complexity is O(n * k).
 
-        return s
-        
+        Space Complexity:
+        The space complexity is O(1) for the in-place modification of the string,
+        but since strings are immutable in Python, each replace operation creates a new string.
+        Therefore, the space complexity is O(n) due to the storage of intermediate strings.
+        """
+        stack = []
+
+        for ch in s:
+            stack.append(ch)
+
+            if len(stack) >= len(part):
+                if "".join(stack[-len(part) :]) == part:
+                    for _ in range(len(part)):
+                        stack.pop()
+
+        return "".join(stack)
+
+
+solution = Solution()
+
+print(solution.removeOccurrences("daabcbaabcbc", "abc"))
