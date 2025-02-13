a0c95f3d3bd16d5c3447a348ecdb9610b6b17ff0
Sagar Neeli <sagarneeli1191@gmail.com>
2025-02-13 17:43:34 -0500

    Time: 27 ms (47.59%) | Memory: 17.8 MB (84.95%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -3,24 +3,33 @@ class Solution:
         if len(s1) > len(s2):
             return False
 
-        s1_map = defaultdict(int)
-        for ch in s1:
-            s1_map[ch] += 1
-
-        s2_map = defaultdict(int)
-        L, k = 0, len(s1)
+        s1_map = Counter(s1)
+        L, R, k = 0, 0, len(s1)
+        missing = len(s1_map)
         
-        for R, ch in enumerate(s2):
-            s2_map[ch] += 1
+        while R < len(s2):
+            if R - L == k:
+                ch = s2[L]
+                if ch in s1_map:
+                    s1_map[ch] += 1
+                    if s1_map[ch] == 1:
+                        missing += 1
+                L += 1
+            
+            ch = s2[R]
+            if ch in s1_map:
+                s1_map[ch] -= 1
+                if s1_map[ch] == 0:
+                    missing -= 1
+            
+            R += 1
 
-            if R - L + 1 == k:
-                if s1_map == s2_map:
-                    return True
-                else:
-                    s2_map[s2[L]] -= 1
-                    if s2_map[s2[L]] == 0:
-                        del s2_map[s2[L]]
-                    L += 1
+            if missing == 0:
+                return True
+
+
+        return False
+
         
         return False
 
