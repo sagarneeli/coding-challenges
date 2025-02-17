47abd60b9697e5f0afa9ccbb28bf8b586e207966
Sagar Neeli <sagarneeli1191@gmail.com>
2025-02-17 16:56:25 -0500

    Time: 67 ms (84.30%) | Memory: 26.4 MB (82.47%) - LeetSync

---
+++ 931-maximum-frequency-stack/maximum-frequency-stack.py
@@ -0,0 +1,27 @@
+class FreqStack:
+
+    def __init__(self):
+        self.freq = defaultdict(int)
+        self.group = defaultdict(list)
+        self.max_freq = 0
+
+    def push(self, val: int) -> None:
+        self.freq[val] += 1
+        freq = self.freq[val]
+        self.max_freq = max(self.max_freq, freq)
+        self.group[freq].append(val)
+
+    def pop(self) -> int:
+        val = self.group[self.max_freq].pop()
+        self.freq[val] -= 1
+        if not self.group[self.max_freq]:
+            self.max_freq -= 1
+        return val
+
+        
+
+
+# Your FreqStack object will be instantiated and called as such:
+# obj = FreqStack()
+# obj.push(val)
+# param_2 = obj.pop()
