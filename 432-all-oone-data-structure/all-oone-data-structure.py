85b4da1baeb2a6bcbc3ef3777534e972968ecd67
Sagar Neeli <sagarneeli1191@gmail.com>
2025-02-27 12:06:48 -0500

    Time: 66 ms (64.79%) | Memory: 34.5 MB (56.75%) - LeetSync

--- 432-all-oone-data-structure/all-oone-data-structure.py
+++ 432-all-oone-data-structure/all-oone-data-structure.py
@@ -7,72 +7,80 @@ class Node:
 
 class AllOne:
     def __init__(self):
-        self.head = Node(0)  # Dummy head
-        self.tail = Node(0)  # Dummy tail
+        self.map = defaultdict(Node)
+        self.head = Node(0)
+        self.tail = Node(0)
         self.head.next = self.tail
         self.tail.prev = self.head
-        self.map = {}  # key -> Node mapping
-
-    def _add_node_after(self, prev_node, new_node):
-        new_node.prev = prev_node
-        new_node.next = prev_node.next
-        prev_node.next.prev = new_node
-        prev_node.next = new_node
-
-    def _remove_node(self, node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
 
     def inc(self, key: str) -> None:
         if key in self.map:
             node = self.map[key]
+            if node.next != self.tail and node.next.freq == node.freq + 1:
+                node.next.keys.add(key)
+                self.map[key] = node.next
+            else:
+                new_node = Node(node.freq + 1)
+                new_node.keys.add(key)
+                self.map[key] = new_node
+                self._add_node_after(node, new_node)
+
             node.keys.remove(key)
-            new_freq = node.freq + 1
-            nxt = node.next
-            if nxt == self.tail or nxt.freq != new_freq:
-                nxt = Node(new_freq)
-                self._add_node_after(node, nxt)
-            nxt.keys.add(key)
-            self.map[key] = nxt
             if not node.keys:
                 self._remove_node(node)
         else:
-            # Key doesn't exist; add to the first node (or create one)
-            if self.head.next == self.tail or self.head.next.freq > 1:
+            first = self.head.next
+            if first == self.tail or first.freq > 1:
                 new_node = Node(1)
+                new_node.keys.add(key)
+                self.map[key] = new_node
                 self._add_node_after(self.head, new_node)
             else:
-                new_node = self.head.next
-            new_node.keys.add(key)
-            self.map[key] = new_node
+                first.keys.add(key)
+                self.map[key] = first
 
     def dec(self, key: str) -> None:
         if key not in self.map:
             return
 
         node = self.map[key]
-        node.keys.remove(key)
         if node.freq == 1:
-            del self.map[key]
+            del self.map[key]   
         else:
-            # Try to use the previous node or create one if needed
-            if node.prev == self.head or node.prev.freq != node.freq - 1:
-                new_node = Node(node.freq - 1)
-                self._add_node_after(node.prev, new_node)
-                target = new_node
+            if node.prev != self.head and node.prev.freq == node.freq - 1:
+                node.prev.keys.add(key)
+                self.map[key] = node.prev
             else:
-                target = node.prev
-            target.keys.add(key)
-            self.map[key] = target
+                new_node = Node(node.freq - 1)
+                new_node.keys.add(key)
+                self.map[key] = new_node
+                self._add_node_after(node.prev, new_node)
 
+        node.keys.remove(key)
         if not node.keys:
             self._remove_node(node)
 
     def getMaxKey(self) -> str:
-        return "" if self.tail.prev == self.head else next(iter(self.tail.prev.keys))
+        if self.tail.prev == self.head:
+            return ""
+        else:
+            return next(iter(self.tail.prev.keys))
 
     def getMinKey(self) -> str:
-        return "" if self.head.next == self.tail else next(iter(self.head.next.keys))
+        if self.head.next == self.tail:
+            return ""
+        else:
+            return next(iter(self.head.next.keys))
+
+    def _add_node_after(self, node: Node, new_node: Node) -> None:
+        new_node.prev = node
+        new_node.next = node.next
+        node.next.prev = new_node
+        node.next = new_node
+
+    def _remove_node(self, node: Node) -> None:
+        node.prev.next = node.next
+        node.next.prev = node.prev
         
 
 
