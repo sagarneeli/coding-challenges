5a0f56e5eea4d33d581a20509825ff96c31f6411
Sagar Neeli <sagarneeli1191@gmail.com>
2025-02-27 15:13:34 -0500

    Time: 328 ms (13.64%) | Memory: 69.7 MB (23.39%) - LeetSync

--- 716-max-stack/max-stack.py
+++ 716-max-stack/max-stack.py
@@ -1,92 +1,82 @@
-from typing import TypeVar, Generic, Optional
-from sortedcontainers import SortedDict  # pip install sortedcontainers
-import threading
+from sortedcontainers import SortedDict
 
-T = TypeVar('T')
+class Node:
 
-class Node(Generic[T]):
-    def __init__(self, val: T):
-        self.val: T = val
-        self.prev: Optional['Node[T]'] = None
-        self.next: Optional['Node[T]'] = None
+    def __init__(self, val):
+        self.val = val
+        self.prev = None
+        self.next = None
 
-class MaxStack(Generic[T]):
+
+class MaxStack:
+
     def __init__(self):
-        # Dummy head and tail nodes for the doubly-linked list.
-        self.head: Node[T] = Node(None)  # type: ignore
-        self.tail: Node[T] = Node(None)  # type: ignore
+        self.head = Node(0)
+        self.tail = Node(0)
         self.head.next = self.tail
         self.tail.prev = self.head
+        self.map = SortedDict()
         
-        # SortedDict mapping value -> list of nodes with that value.
-        self.map: SortedDict[T, list[Node[T]]] = SortedDict()
+    def push(self, x: int) -> None:
+        node = Node(x)
+        self._add_node(node)
         
-        # Lock for thread safety.
-        self.lock = threading.RLock()
+        if x in self.map:
+            self.map[x].append(node)
+        else:
+            self.map[x] = [node]
 
-    def push(self, to_push: T) -> None:
-        with self.lock:
-            node = Node(to_push)
-            self._add_node(node)
-            if to_push in self.map:
-                self.map[to_push].append(node)
-            else:
-                self.map[to_push] = [node]
 
-    def top(self) -> T:
-        with self.lock:
-            if self.head.next == self.tail:
-                raise IndexError("peek from empty stack")
-            return self.tail.prev.val
+    def pop(self) -> int:
+        if self.head.next == self.tail:
+            return -1
 
-    def pop(self) -> T:
-        with self.lock:
-            if self.head.next == self.tail:
-                raise IndexError("pop from empty stack")
-            node = self.tail.prev
-            self._remove_node(node)
-            nodes_list = self.map[node.val]
-            nodes_list.remove(node)
-            if not nodes_list:
-                del self.map[node.val]
-            return node.val
+        node = self.tail.prev
+        self._remove_node(node)
+        node_list = self.map[node.val]
+        node_list.remove(node)
 
-    def peekMax(self) -> T:
-        with self.lock:
-            if not self.map:
-                raise IndexError("peekMax from empty stack")
-            return self.map.peekitem(-1)[0]
+        if not node_list:
+            del self.map[node.val]
 
-    def popMax(self) -> T:
-        with self.lock:
-            if not self.map:
-                raise IndexError("popMax from empty stack")
-            max_val, nodes_list = self.map.peekitem(-1)
-            node = nodes_list.pop()
-            if not nodes_list:
-                del self.map[max_val]
-            self._remove_node(node)
-            return node.val
+        return node.val
+        
 
-    def _add_node(self, node: Node[T]) -> None:
+    def top(self) -> int:
+        if self.head.next == self.tail:
+            return -1
+        return self.tail.prev.val
+        
+
+    def peekMax(self) -> int:
+        if not self.map:
+            return -1
+        return self.map.peekitem(-1)[0]
+        
+
+    def popMax(self) -> int:
+        if not self.map:
+            return -1
+
+        max_val, node_list = self.map.peekitem(-1)
+        node = node_list.pop()
+        if not node_list:
+            del self.map[max_val]
+        
+        self._remove_node(node)
+        return node.val
+
+    
+    def _add_node(self, node: Node) -> None:
         node.prev = self.tail.prev
         node.next = self.tail
         self.tail.prev.next = node
         self.tail.prev = node
+    
 
-    def _remove_node(self, node: Node[T]) -> None:
+    def _remove_node(self, node: Node) -> None:
         node.prev.next = node.next
         node.next.prev = node.prev
-
-    # Helper method to visualize the current stack from bottom to top.
-    def get_stack_elements(self) -> list[T]:
-        elements = []
-        cur = self.head.next
-        while cur != self.tail:
-            elements.append(cur.val)
-            cur = cur.next
-        return elements
-
 
 # Your MaxStack object will be instantiated and called as such:
 # obj = MaxStack()
