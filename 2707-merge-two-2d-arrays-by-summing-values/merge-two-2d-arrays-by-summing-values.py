528f79c2a8331c640742cc7032cb94acaa388d6b
Sagar Neeli <sagarneeli1191@gmail.com>
2025-03-03 10:00:38 -0500

    Time: 1 ms (37.31%) | Memory: 18 MB (38.07%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.py
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.py
@@ -1,18 +1,17 @@
 class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
-        key_to_sum = {}
+        num_map = defaultdict(int)
 
-        # Copying the array nums1 to the map.
-        for nums in nums1:
-            key_to_sum[nums[0]] = nums[1]
+        for id, val in nums1:
+            num_map[id] = val
+        
+        for id, val in nums2:
+            num_map[id] += val
 
-        # Adding the values to existing keys or create new entries.
-        for nums in nums2:
-            key_to_sum[nums[0]] = key_to_sum.get(nums[0], 0) + nums[1]
+        result = []
+        for id, val in sorted(num_map.items()):
+            result.append([id, val])
 
-        merged_array = []
-        for key, value in sorted(key_to_sum.items()):
-            merged_array.append([key, value])
+        return result
 
-        return merged_array
         
